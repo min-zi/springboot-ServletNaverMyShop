c907c48c6be063bb86b81e11cb538acc2131be4e
min-zi <min-zi@naver.com>
2022-05-26 23:24:23 +0900

    Controller 없이 WepServlet 동작 시키기

---
+++ src/main/java/com/ming/snavermyshop/SNaverMyShopApplication.java
@@ -0,0 +1,15 @@
+package com.ming.snavermyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
+@SpringBootApplication
+public class SNaverMyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SNaverMyShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/snavermyshop/SNaverMyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.snavermyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SNaverMyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
