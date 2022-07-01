e7578e503ba0623d38d124a9b410031605f3d56f
min-zi <min-zi@naver.com>
2022-07-02 07:32:20 +0900

    Feat: 로그인/로그아웃 기능 추가

---
+++ src/main/java/com/ming/snavermyshop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ming.snavermyshop.controller;
+
+import com.ming.snavermyshop.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+        return "index";
+    }
+}
