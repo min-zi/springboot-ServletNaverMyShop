01f0ce4bb35a55b2340eca36e413794ad0792d17
min-zi <min-zi@naver.com>
2022-07-15 23:46:10 +0900

    test: Edge 케이스 test code

--- src/main/java/com/ming/snavermyshop/model/Product.java
+++ src/main/java/com/ming/snavermyshop/model/Product.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줌
@@ -39,6 +42,27 @@ public class Product {
 
     // 관심 상품 만들어주는 생성자
     public Product(ProductRequestDto requestDto, Long userId) {
+        // 입력값 Validation
+        if (userId == null || userId <= 0) {
+            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+        }
+
+        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getImage())) {
+            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getLink())) {
+            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (requestDto.getLprice() <= 0) {
+            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+        }
+
         // 관심상품을 등록한 회원 테이블 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
@@ -46,5 +70,19 @@ public class Product {
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
+    }
+
+    boolean isValidUrl(String url)
+    {
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (URISyntaxException exception) {
+            return false;
+        }
+        catch (MalformedURLException exception) {
+            return false;
+        }
     }
 }
