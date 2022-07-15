bb68a870d90bc6eea97478e58b401d56be1d8976
min-zi <min-zi@naver.com>
2022-07-16 03:47:03 +0900

    refactor: 리팩토링 test code

--- src/main/java/com/ming/snavermyshop/model/Product.java
+++ src/main/java/com/ming/snavermyshop/model/Product.java
@@ -1,9 +1,11 @@
 package com.ming.snavermyshop.model;
 
 import com.ming.snavermyshop.dto.ProductRequestDto;
+import com.ming.snavermyshop.validator.ProductValidator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.parameters.P;
 
 import javax.persistence.*;
 import java.net.MalformedURLException;
@@ -42,26 +44,7 @@ public class Product {
 
     // 관심 상품 만들어주는 생성자
     public Product(ProductRequestDto requestDto, Long userId) {
-        // 입력값 Validation
-        if (userId == null || userId <= 0) {
-            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
-        }
-
-        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
-            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
-        }
-
-        if (!isValidUrl(requestDto.getImage())) {
-            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (!isValidUrl(requestDto.getLink())) {
-            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (requestDto.getLprice() <= 0) {
-            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
-        }
+        ProductValidator.validateProductInput(requestDto, userId);
 
         // 관심상품을 등록한 회원 테이블 Id 저장
         this.userId = userId;
@@ -70,19 +53,5 @@ public class Product {
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
-    }
-
-    boolean isValidUrl(String url)
-    {
-        try {
-            new URL(url).toURI();
-            return true;
-        }
-        catch (URISyntaxException exception) {
-            return false;
-        }
-        catch (MalformedURLException exception) {
-            return false;
-        }
     }
 }
