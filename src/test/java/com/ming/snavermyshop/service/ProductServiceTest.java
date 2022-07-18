1d37e9c9593ffbbee6300d5b554c4a4c9ba37dbd
min-zi <min-zi@naver.com>
2022-07-19 05:09:14 +0900

    test: Mockito 단위테스트
    Mockito framework 사용해서 테스트 해봄

---
+++ src/test/java/com/ming/snavermyshop/service/ProductServiceTest.java
@@ -0,0 +1,81 @@
+package com.ming.snavermyshop.service;
+
+import com.ming.snavermyshop.dto.ProductMypriceRequestDto;
+import com.ming.snavermyshop.dto.ProductRequestDto;
+import com.ming.snavermyshop.model.Product;
+import com.ming.snavermyshop.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static com.ming.snavermyshop.service.ProductService.MIN_MY_PRICE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @Mock // 가짜 객체가 자동으로 생성
+    ProductRepository productRepository;
+
+    @Test
+    @DisplayName("관심 상품 희망가 - 최저가 이상으로 변경")
+    void updateProduct_Normal() {
+        // given
+        Long productId = 100L;
+        int myprice = MIN_MY_PRICE + 1000;
+
+        ProductMypriceRequestDto requestMyPriceDto = new ProductMypriceRequestDto(
+                myprice
+        );
+
+        Long userId = 777L;
+        ProductRequestDto requestProductDto = new ProductRequestDto(
+                "오리온 꼬북칩 초코츄러스맛 160g",
+                "https://shopping-phinf.pstatic.net/main_2416122/24161228524.20200915151118.jpg",
+                "https://search.shopping.naver.com/gate.nhn?id=24161228524",
+                2350
+        );
+
+        Product product = new Product(requestProductDto, userId);
+
+        ProductService productService = new ProductService(productRepository);
+        when(productRepository.findById(productId)) // input
+                .thenReturn(Optional.of(product)); // output 결과에 대해 명시해줘야 됨 (findById 의 리턴타입이 Optional 임)
+
+        // when
+        Product result = productService.updateProduct(productId, requestMyPriceDto);
+
+        // then
+        assertEquals(myprice, result.getMyprice());
+    }
+
+    @Test
+    @DisplayName("관심 상품 희망가 - 최저가 미만으로 변경")
+    void updateProduct_Failed() {
+        // given
+        Long productId = 100L;
+        int myprice = MIN_MY_PRICE - 50;
+
+        ProductMypriceRequestDto requestMyPriceDto = new ProductMypriceRequestDto(
+                myprice
+        );
+
+        ProductService productService = new ProductService(productRepository);
+
+        // when
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            productService.updateProduct(productId, requestMyPriceDto);
+        });
+
+        // then
+        assertEquals(
+                "유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요.",
+                exception.getMessage()
+        );
+    }
+}
